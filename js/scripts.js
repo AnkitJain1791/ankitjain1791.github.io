199aad2a81f8067a8766421f56fcf805e469ab6e
AnkitJain1791 <45723303+AnkitJain1791@users.noreply.github.com>
2018-12-14 18:12:55 -0500

    Maps

--- js/scripts.js
+++ js/scripts.js
@@ -237,7 +237,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var itc_kol = {lat: 22.5932759, lng: 88.27027720000001};
+    var itc_kol = {lat: 28.622223, lng: 77.118101};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: itc_kol,
@@ -251,7 +251,7 @@ function initMap() {
 }
 
 function initBBSRMap() {
-    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
+    var la_fiesta = {lat: 28.622223, lng: 77.118101};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: la_fiesta,
